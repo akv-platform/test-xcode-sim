e62997880926d31b07fd1f04488115984e0e565f
Sergey Dolin <v-sedoli@microsoft.com>
2018-09-03 13:10:06 +0500

    exit on build error

--- build_all.sh
+++ build_all.sh
@@ -5,5 +5,8 @@ for xcode in $XCODES;do
 	project=`echo $xcode|tr [:upper:] [:lower:]|sed 's/[_.]//g'`
 	target=`basename $project`
 	$script_dir/build.sh "$xcode" "$project" "$target" Release
+	ec=$?
+	test $ec || exit $ec
 	$script_dir/build.sh "$xcode" "$project" "$target" Debug
+	test $ec || exit $ec
 done
