a7278487413ae4493755249b4edd941389dcac2e
Sergey Dolin <v-sedoli@microsoft.com>
2018-09-03 10:46:25 +0500

    Add build xcode project script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+XCODE=$1
+PROJECT=$2
+TARGET=$3
+CONFIGURATION=$4
+
+xcode-select -s "/Application/$XCODE"
+cd "$PRODJECT"
+xcodebuild clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -target $3 -configuration $4
