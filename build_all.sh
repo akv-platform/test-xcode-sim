e3df5f36308aca7d0f6d78809612fb74b55c4159
Sergey Dolin <v-sedoli@microsoft.com>
2018-09-03 12:54:31 +0500

    remove undescores and dots from file name

--- build_all.sh
+++ build_all.sh
@@ -2,7 +2,7 @@
 script_dir=`dirname $0`
 . "$script_dir/xcodes.sh"
 for xcode in $XCODES;do
-	project=`echo $xcode|tr [:upper:] [:lower:]`
+	project=`echo $xcode|tr [:upper:] [:lower:]|sed 's/[_.]//g'`
 	target=`basename $project`
 	$script_dir/build.sh "$xcode" "$project" "$target" Release
 	$script_dir/build.sh "$xcode" "$project" "$target" Debug
