918121419a9f53617e23b3c01ed977d370ca6c8c
Sergey Dolin <v-sedoli@microsoft.com>
2018-09-03 10:47:43 +0500

    add .app to xcode foler

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ PROJECT=$2
 TARGET=$3
 CONFIGURATION=$4
 
-xcode-select -s "/Application/$XCODE"
+xcode-select -s "/Application/$XCODE.app"
 cd "$PRODJECT"
 xcodebuild clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -target $3 -configuration $4
